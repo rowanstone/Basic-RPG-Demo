13ef2697719e241212776dfdffaca5184dc55583
Rowan Stone <43486575+sethkimaris@users.noreply.github.com>
2023-02-08 18:19:26 -0600

    Add files via upload

---
+++ project-files/CS4381-RPG/Assets/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class ChangeScene : MonoBehaviour
+{ 
+    public void LoadNextScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
